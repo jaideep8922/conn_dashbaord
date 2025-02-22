28720e914964e68d5a29cefc3f299d29770d0d70
Jai892289 <gjai4341@gmail.com>
2025-02-23 02:22:07 +0530

    all

--- src/component/UserList.tsx
+++ src/component/UserList.tsx
@@ -9,39 +9,39 @@ export default function UserList() {
 
   const [count, setCount] = useState<any>(0)
   useEffect(() => {
-        const fetchProductList = async () => {
-            try {
-                const response = await fetch(
-                    `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/get-dashboard-counts`,
-                    {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                    }
-                );
+    const fetchProductList = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/get-dashboard-counts`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
-                const result = await response.json();
-                console.log("result", result)
+        const result = await response.json();
+        console.log("result", result)
 
-                if (!response.ok) {
-                    throw new Error(result.message || 'Failed to fetch product details');
-                }
+        if (!response.ok) {
+          throw new Error(result.message || 'Failed to fetch product details');
+        }
 
-                setCount(result)
-                // setProductData(result);
-                // setError(null);
-            } catch (error) {
-              if (error instanceof Error) {
-                console.error('Error fetching product list:', error.message);
-              }
-                // setError(error.message);
-            }
-        };
+        setCount(result)
+        // setProductData(result);
+        // setError(null);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error fetching product list:', error.message);
+        }
+        // setError(error.message);
+      }
+    };
 
-        fetchProductList();
-    
-}, []);
+    fetchProductList();
+
+  }, []);
 
 
   return (
@@ -89,7 +89,7 @@ export default function UserList() {
           <div className="p-2">
             <h3 className="font-semibold mb-4"><span className="text-2xl font-bold pr-3">{count?.data?.activeUsersCount}</span>Total Users</h3>
             {/* <LineChart /> */}
-            <AreaChart data={[{ name: "Total Users", value:  0 },{ name: "Total Users", value: count?.data?.activeUsersCount || 0 }]} />
+            <AreaChart data={[{ name: "Total Users", value: 0 }, { name: "Total Users", value: count?.data?.activeUsersCount || 0 }]} />
 
           </div>
         </div>
@@ -98,7 +98,7 @@ export default function UserList() {
           <div className="p-4">
             <h3 className="font-semibold mb-4"><span className="text-2xl font-bold pr-3">{count?.data?.registeredProducts}</span>Registered Product</h3>
             {/* <LineChart variant="yellow" /> */}
-            <AreaChart data={[{ name: "Total Registered Product", value:0 },{ name: "Total Registered Product", value: count?.data?.registeredProducts || 0 }]} />
+            <AreaChart data={[{ name: "Total Registered Product", value: 0 }, { name: "Total Registered Product", value: count?.data?.registeredProducts || 0 }]} />
 
           </div>
         </div>
@@ -120,17 +120,17 @@ export default function UserList() {
         </div>
       </div> */}
 
-<div className="grid grid-cols-1 gap-4">
-  <div>
-    <div className="p-6">
-      <div className="flex items-center justify-between mb-4">
-        <h3 className="font-semibold mb-4">Enquiries<span className="text-2xl font-bold pl-3 text-yellow-400">{count?.data?.enquiresCount}</span></h3>
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <div className="p-6">
+            <div className="flex items-center justify-between mb-4">
+              <h3 className="font-semibold mb-4">Enquiries<span className="text-2xl font-bold pl-3 text-yellow-400">{count?.data?.enquiresCount}</span></h3>
+            </div>
+            {/* Check if registeredProducts exists and is an array */}
+            <DonutChart registeredProducts={count?.data?.enquiresCount || [0, 0, 0, 0, 0]} />
+          </div>
+        </div>
       </div>
-      {/* Check if registeredProducts exists and is an array */}
-      <DonutChart registeredProducts={count?.data?.enquiresCount || [0, 0, 0, 0, 0]} />
-    </div>
-  </div>
-</div>
 
     </div>
   )
